28325de88563d2a683a4c1f84b59b6bf14950658
sirhorseyinfo <162401234+sirhorseyinfo@users.noreply.github.com>
2024-11-25 17:30:55 +0000

    Remote from codespaces

--- main.sh
+++ main.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 
-prompt_type="prompt-types.json"
-
 echo "(AI): Hi, I'm Shelly! I'm a responsive AI built primarily from shell.
 How can I help you today?"
-read -r input_keyword
 
-value=$(jq -r --arg keyword "$input_keyword" '.[$keyword] // "unknown"' "$prompt_type")3
+sleep 0.3
+echo "(!Err!): Timeout."
+sleep 1.2
+
+exit
