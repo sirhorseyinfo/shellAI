3c1da99348bc194fb9fc5e20c31fde1dc57ab818
sirhorseyinfo <162401234+sirhorseyinfo@users.noreply.github.com>
2024-11-25 20:02:50 +0000

    Remote from codespaces

--- save.sh
+++ save.sh
@@ -1,10 +1,34 @@
 #/bin/bash
 # This file is for saving to github from codespaces
 
-#echo "Do you need to rabase? [y/n]"
-#read message
+runrebase() {
+    git pull --rebase
+    git add .
+    git commit -m "Remote from codespaces"
+    git push origin main
+    sleep 0.3
+    clear
 
-#if [ $message = "y" ] || [ $message = "Y" ]
+    sleep 0.3
+    echo "Saved."
+    sleep 1.3
+    clear
+}
+
+echo "Do you need to rabase? [y/n]"
+read message
+
+if [ $message = "y" ] || [ $message = "Y" ]; then
+  echo "Running with git rebase..."
+  sleep 1
+
+  runrebase
+  exit
+elif [ $message = "n" ] || [ $message = "N" ]; then
+  echo "Proceding to main file..."
+else 
+  echo "Not a valid responce. Proceding to main file..."
+fi
 
 git add .
 git commit -m "Remote from codespaces"
@@ -15,5 +39,4 @@ clear
 sleep 0.3
 echo "Saved."
 sleep 1.3
-
 clear
