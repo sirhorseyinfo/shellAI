cb6cbd88d4b2d16c5724e684eed5d321f561d5a9
sirhorseyinfo <162401234+sirhorseyinfo@users.noreply.github.com>
2024-11-25 17:13:43 +0000

    Remote from codespaces

---
+++ save.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+# This file is for saving to github from codespaces
+
+git add .
+git commit -m "Remote from codespaces"
+git push origin main
+clear
+
+sleep 1
+echo "Saved."
+sleep 1
+
+clear
